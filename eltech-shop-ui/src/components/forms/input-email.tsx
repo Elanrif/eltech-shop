b5e87f0796e5ef3a6750e33e3bf3f62cf2f99ac7
Elanrif <saidbacoelanrif@gmail.com>
2025-01-14 13:36:34 +0100

    feat: input with icon use relative and absolute

--- eltech-shop-ui/src/components/forms/input-email.tsx
+++ eltech-shop-ui/src/components/forms/input-email.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import { Input } from '../ui/input'
-import { Facebook } from 'lucide-react'
+import { Mail } from 'lucide-react'
 
 function InputEmail(
     {className, ...props}: React.ComponentPropsWithoutRef<"input"> & {className?:string},ref: React.Ref<HTMLInputElement>
 ) {
-    return <Input icon={<Facebook />} ref={ref} className={className} {...props} />;
+    return <Input icon={<Mail className='size-1'/>} ref={ref} className={className} {...props} />;
 }
 
 export default React.forwardRef(InputEmail);
--- eltech-shop-ui/src/components/ui/input.tsx
+++ eltech-shop-ui/src/components/ui/input.tsx
@@ -5,19 +5,25 @@ import { cn } from "@/lib/utils"
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input"> & {icon?: React.JSX.Element}>(
   ({ className, type, icon, ...props }, ref) => {
     return (
-      <div>
+      <div
+        className={cn(
+          "flex justify-between relative",
+          className
+        )}
+      >
         <input
           type={type}
           className={cn(
-            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus:ring-2 focus:ring-bg focus-visible:ring-5 focus-visible:ring-shop-secondary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "group flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus:ring-2 focus:ring-bg focus-visible:ring-5 focus-visible:ring-shop-secondary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
             className
           )}
           ref={ref}
           {...props}
         />
-        {icon && React.cloneElement(icon, {
-          className: cn("absolute rigt-3 text-shop-muted flex items-center"),
-        })}
+        {icon &&
+          React.cloneElement(icon, {
+            className: cn("absolute right-2 size-5 top-2 text-shop-muted"),
+          })}
       </div>
     );
   }
