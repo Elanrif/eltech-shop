7b8bb52a988177a10b8899d569fcca252f145ef9
Elanrif <saidbacoelanrif@gmail.com>
2025-01-14 13:55:23 +0100

    fix: input-email.tsx we have to extends our type.

--- eltech-shop-ui/src/components/forms/input-email.tsx
+++ eltech-shop-ui/src/components/forms/input-email.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react'
 import { Input } from '../ui/input'
 import { Mail } from 'lucide-react'
+import { cn } from '@/lib/utils';
 
+export type PropsEmail = {
+    variant: 'base' | 'md' | 'lg' | 'xl';
+    className: string;
+}
 function InputEmail(
-    {className, ...props}: React.ComponentPropsWithoutRef<"input"> & {className?:string},ref: React.Ref<HTMLInputElement>
+    {variant = 'lg', className, ...props}: React.ComponentPropsWithoutRef<"input"> & {className?:string},ref: React.Ref<HTMLInputElement>
 ) {
-    return <Input icon={<Mail className='size-1'/>} ref={ref} className={className} {...props} />;
+    const variantClass = {
+      base: "text-sm py-2 px-3",
+      md: "text-md py-2.5 px-4",
+      lg: "text-lg py-3 px-5",
+      xl: "text-xl py-4 px-6",
+    }[variant];
+    
+    return (
+      <Input
+        icon={<Mail className="size-1" />}
+        ref={ref}
+        className={cn(className,variantClass)}
+        {...props}
+      />
+    );
 }
 
 export default React.forwardRef(InputEmail);
