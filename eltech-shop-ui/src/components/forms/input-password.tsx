7d9e2787f063e8a872645b41df674a5ece6a316e
Elanrif <saidbacoelanrif@gmail.com>
2025-01-14 16:44:57 +0100

    feat: diplay password

--- eltech-shop-ui/src/components/forms/input-password.tsx
+++ eltech-shop-ui/src/components/forms/input-password.tsx
@@ -1,15 +1,21 @@
+"use client";
 import * as React from 'react'
 import { Input } from '../ui/input'
-import { Lock } from 'lucide-react'
+import { Lock, Unlock } from 'lucide-react'
 import { cn } from '@/lib/utils';
+import TypographyLabel from '../ui/typography-label';
 
-export type PropsEmail = {
+export type PropsPassword = {
     variant?: 'base' | 'md' | 'lg' | 'xl';
     className?: string;
 }
 function InputPassword(
-    {variant = 'lg', className, ...props}: React.ComponentPropsWithoutRef<"input"> & PropsEmail ,ref: React.Ref<HTMLInputElement>
+    {variant = 'lg', className, ...props}: React.ComponentPropsWithoutRef<"input"> & PropsPassword ,ref: React.Ref<HTMLInputElement>
 ) {
+    const [isChecked, setIsChecked] = React.useState(false);
+    const handleCheckboxChange = () => {
+      setIsChecked((prev) => !prev);
+    };
     const variantClass = {
       base: "text-sm py-4 xs:w-[10rem]",
       md: "text-md md:w-[30rem] py-5",
@@ -17,14 +23,28 @@ function InputPassword(
       xl: "py-4 px-6 w-full xl:w-[64rem] py-5 px-5",
     }[variant];
     
+    const typeInput = isChecked ? 'text': 'password';
+    const lockIcon = isChecked ? Unlock : Lock
+
     return (
-      <Input
-        type='password'
-        icon={Lock}
-        ref={ref}
-        className={cn(className,variantClass)}
-        {...props}
-      />
+      <div>
+        <Input
+          type={typeInput}
+          icon={lockIcon}
+          ref={ref}
+          className={cn(className, variantClass)}
+          {...props}
+        />
+        <div className="flex gap-2 mt-3 items-center">
+          <input
+            id="display-pwd"
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+          />
+          <TypographyLabel htmlFor='display-pwd' value="afficher le mot de passe" className="mt-0" />
+        </div>
+      </div>
     );
 }
 
