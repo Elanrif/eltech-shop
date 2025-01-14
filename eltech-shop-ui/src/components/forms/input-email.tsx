c61384718668f844c32a4f116725f012545e47a3
Elanrif <saidbacoelanrif@gmail.com>
2025-01-14 14:12:53 +0100

    feat: correct the input type icon, we have to put the right implementation

--- eltech-shop-ui/src/components/forms/input-email.tsx
+++ eltech-shop-ui/src/components/forms/input-email.tsx
@@ -4,22 +4,22 @@ import { Mail } from 'lucide-react'
 import { cn } from '@/lib/utils';
 
 export type PropsEmail = {
-    variant: 'base' | 'md' | 'lg' | 'xl';
-    className: string;
+    variant?: 'base' | 'md' | 'lg' | 'xl';
+    className?: string;
 }
 function InputEmail(
-    {variant = 'lg', className, ...props}: React.ComponentPropsWithoutRef<"input"> & PropsEmail & {className?:string},ref: React.Ref<HTMLInputElement>
+    {variant = 'lg', className, ...props}: React.ComponentPropsWithoutRef<"input"> & PropsEmail ,ref: React.Ref<HTMLInputElement>
 ) {
     const variantClass = {
       base: "text-sm py-2 px-3",
       md: "text-md py-2.5 px-4",
-      lg: "text-lg py-3 px-5",
+      lg: "text-lg py-12 px-5",
       xl: "text-xl py-4 px-6",
     }[variant];
     
     return (
       <Input
-        icon={<Mail className="size-1" />}
+        icon={<Mail className="size-5" />}
         ref={ref}
         className={cn(className,variantClass)}
         {...props}
--- eltech-shop-ui/src/components/ui/input.tsx
+++ eltech-shop-ui/src/components/ui/input.tsx
@@ -1,33 +1,36 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { LucideProps } from "lucide-react";
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input"> & {icon?: React.JSX.Element}>(
-  ({ className, type, icon, ...props }, ref) => {
-    return (
-      <div
+export type PropsInput = {
+  variant?: "base" | "md" | "lg" | "xl";
+  className?: string;
+};
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<"input"> & {
+    icon?: React.ReactElement<LucideProps>;
+  }
+>(({ className, type, icon, ...props }, ref) => {
+  return (
+    <div className={cn("flex justify-between relative", className)}>
+      <input
+        type={type}
         className={cn(
-          "flex justify-between relative",
+          "group flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus:ring-2 focus:ring-bg focus-visible:ring-5 focus-visible:ring-shop-secondary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
-      >
-        <input
-          type={type}
-          className={cn(
-            "group flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus:ring-2 focus:ring-bg focus-visible:ring-5 focus-visible:ring-shop-secondary disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-        {icon &&
-          React.cloneElement(icon, {
-            className: cn("absolute right-2 size-5 top-2 text-shop-muted"),
-          })}
-      </div>
-    );
-  }
-)
+        ref={ref}
+        {...props}
+      />
+      {icon &&
+        React.cloneElement(icon, {
+          className: cn("absolute right-2 size-5 top-2 text-shop-muted"),
+        })}
+    </div>
+  );
+});
 Input.displayName = "Input"
 
 export { Input }
