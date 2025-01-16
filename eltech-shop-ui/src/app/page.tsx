6f41025e14f8fdfc36817f2c1765030570d5ecb4
Elanrif <saidbacoelanrif@gmail.com>
2025-01-16 08:45:47 +0100

    feat: hydration error

--- eltech-shop-ui/src/app/page.tsx
+++ eltech-shop-ui/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home
+    <div>
+      <Input placeholder='Entrer votre nom...' className='m-10 max-w-[20rem] px-3 py-4'/>
     </div>
   )
 }
