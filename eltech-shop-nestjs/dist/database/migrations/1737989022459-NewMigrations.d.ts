defe2c56f2922e45b35b5d824ef3e3574c4f3bbe
Elanrif <saidbacoelanrif@gmail.com>
2025-01-27 17:24:14 +0100

    feat: apply transformer and class validator

---
+++ eltech-shop-nestjs/dist/database/migrations/1737989022459-NewMigrations.d.ts
@@ -0,0 +1,6 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+export declare class NewMigrations1737989022459 implements MigrationInterface {
+    name: string;
+    up(queryRunner: QueryRunner): Promise<void>;
+    down(queryRunner: QueryRunner): Promise<void>;
+}
--- eltech-shop-nestjs/src/users/dto/create-user.dto.ts
+++ eltech-shop-nestjs/src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsArray, IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { Basket } from "src/baskets/entities/basket.entity";
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsArray()
+  @IsOptional()
+  baskets: Basket[];
+}
